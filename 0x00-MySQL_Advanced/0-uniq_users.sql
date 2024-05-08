33a85b27610bd33990af1e418a34e638ca7c370b
Mohamed Kamal <ku18mz@gmail.com>
2024-05-08 20:19:05 +0300

    task 0, remove database creation line

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,4 @@
 -- Create holberton database
-CREATE DATABASE IF NOT EXISTS holberton;
 CREATE TABLE IF NOT EXISTS holberton.users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
