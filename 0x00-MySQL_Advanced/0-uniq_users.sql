b4d46f25d28db408ed0b9330da165ced6f66191c
Mohamed Kamal <ku18mz@gmail.com>
2024-05-08 20:23:31 +0300

    task 0, make the script executable on any database

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
 -- Create holberton database
-CREATE DATABASE IF NOT EXISTS holberton;
-CREATE TABLE IF NOT EXISTS holberton.users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
