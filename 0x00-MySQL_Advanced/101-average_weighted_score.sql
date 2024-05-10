cea63250e6e71384a8adfc8908c6b40533bc22a3
Mohamed Kamal <ku18mz@gmail.com>
2024-05-10 04:06:04 +0300

    TASK 13, task 12 procedure for all users.

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,20 @@
+-- STORED PROCEDURE: ComputeAverageWeightedScoreForUsers THAT CALCULATES THE AVERAGE WEIGHTED SCORE FOR ALL USERS.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+  DECLARE user_id INT;
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE cur CURSOR FOR SELECT id FROM users;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+  OPEN cur;
+  read_loop: LOOP
+    FETCH cur INTO user_id;
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+    CALL ComputeAverageWeightedScoreForUser(user_id);
+  END LOOP;
+  CLOSE cur;
+END//
+DELIMITER ;
